b538c228dd423f582ec3b14a4bd9e13541c42cee
Alexsander Gonçalves da Silva <alexsander.gon.silva@gmail.com>
2023-09-19 14:14:13 -0300

    atualização

--- galloflix/src/app/pages/home/home.component.ts
+++ galloflix/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 import { MovieApiService } from 'src/app/services/movie-api.service';
 
 @Component({
